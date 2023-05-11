a33764fcd5492513b10a2d6afbad206ef542ce6b
Bilal Pathan <microbilalpthn@gmail.com>
2023-05-11 14:21:22 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -8,7 +8,7 @@ const Login = (props) => {
 
   const HandleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/auth/login/", {
+    const response = await fetch("https://inotebook-ucvf.onrender.com/auth/login/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
